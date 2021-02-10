1189c2fc685498b744d1fd8a968b8bf205f678f2
zeroal3nzi <36914816+zeroal3nzi@users.noreply.github.com>
2021-02-10 14:04:20 -0800

    v 0.1

---
+++ Menu/BasePlate.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using ExSharpBase.Modules;
+using ExSharpBase.Events;
+using SharpDX;
+using ExSharpBase.Cheat;
+
+namespace ExSharpBase.Menu
+{
+    public partial class BasePlate : Form
+    {
+        public BasePlate()
+        {
+            InitializeComponent();
+        }
+
+        private void BasePlate_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                NativeImport.ReleaseCapture();
+                NativeImport.SendMessage(Handle, NativeImport.WM_NCLBUTTONDOWN, NativeImport.HTCAPTION, 0);
+            }
+        }
+        //{ "DrawPlayers", true },
+        //    { "Draw2dBox", true },
+        //    { "Draw3dBox", false },
+        //    { "DrawSkelton", true },
+        //    { "DrawCrosshair", true },
+        //    { "Aimbot", true },
+        //    { "DrawFOV", true },
+        //    { "MagicBullet", true },
+        //    { "DrawLoot", true },
+        //    { "DrawVehicle", true }
+        private void DrawRangeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["DrawPlayers"] = DrawPlayerCheckBox.Checked;
+        }
+
+        private void MoveToMouseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["DrawLoot"] = LootCheckBox.Checked;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["Aimbot"] = AimbotCheckBox.Checked;
+            Aimbot.aimbot = AimbotCheckBox.Checked;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Drawing.FOV = trackBar1.Value;
+            Aimbot.raduisFov = trackBar1.Value;
+        }
+
+        private void btnBoxColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.AllowFullOpen = false;
+            colorDlg.AnyColor = true;
+            colorDlg.SolidColorOnly = false;
+
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                lblBoxColor.ForeColor = colorDlg.Color;
+                var c = colorDlg.Color;
+                Drawing.BoxColor =new Color(c.R,c.G,c.B,c.A);
+            }
+        }
+
+        private void btnSkeltonColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            colorDlg.AllowFullOpen = false;
+            colorDlg.AnyColor = true;
+            colorDlg.SolidColorOnly = false;
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                lblSkeltonColor.ForeColor = colorDlg.Color;
+                var c = colorDlg.Color;
+                Drawing.SkeltonColor = new Color(c.R, c.G, c.B, c.A);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["DrawCrosshair"] = chkDrawCrosshair.Checked;
+        }
+
+        private void chk2dBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["Draw2dBox"] = chk2dBox.Checked;
+        }
+
+        private void chk3dBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["Draw3dBox"] = chk3dBox.Checked;
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["DrawFOV"] = chkDrawFOV.Checked; 
+        }
+
+        private void chkDrawSkelton_CheckedChanged(object sender, EventArgs e)
+        {
+            //DrawSkelton
+            Drawing.DrawingProperties["DrawSkelton"] = chkDrawSkelton.Checked;
+        }
+
+        private void MagicBulletCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            Drawing.DrawingProperties["MagicBullet"] = MagicBulletCheckBox.Checked;
+            Aimbot.SilentAim = MagicBulletCheckBox.Checked;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExSharpBase.Game;
+using ExSharpBase.Modules;
+using Newtonsoft.Json.Linq;
+
+namespace ExSharpBase
+{
+    class Program
+    {
+        public static Overlay.Base DrawBase = new Overlay.Base();
+        public static Menu.BasePlate MenuBasePlate = new Menu.BasePlate();
+        public static Memory Driver = new Memory();
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+
+                await Task.Run(() => Utils.EnterDebugMode());
+                await Task.Run(() => LogService.Log("Loading Driver"));
+                await Task.Run(() => Driver.LoadDriver());
+                await Task.Run(() => Driver.Attach(Utils.GetPubgProcess()));
+                await Task.Run(() => Engine.Inti());
+                await Task.Run(() => LogService.Log("Found Live Instance of The Game."));
+                await Task.Run(() => Events.EventsManager.SubscribeToEvents());
+                await Task.Run(() => LogService.Log("Initialising Overlay Rendering..."));
+                await Task.Run(() => DrawBase.Show());
+
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
